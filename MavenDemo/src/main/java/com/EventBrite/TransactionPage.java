5e0254ff163e9fc3211a2cf45dd90e7c14784a2c
lindsiedinh <lindsiedinh@gmail.com>
2017-11-09 10:28:03 -0600

    Transaction System v0.1

---
+++ MavenDemo/src/main/java/com/EventBrite/TransactionPage.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.EventBrite;
+
+/**
+ *
+ * @author Lindsie Dinh
+ */
+public class TransactionPage {
+    
+    private long amount;
+    private int bankID;
+    
+    
+    public TransactionPage(Event event) {
+        
+        
+        
+        
+    }
+    
+    
+    
+}
---
+++ MavenDemo/src/test/java/com/EventBrite/TransactionPageTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.EventBrite;
+
+import com.EventBrite.*;
+
+/**
+ *
+ * @author Lindsie Dinh
+ */
+public class TransactionPageTest {
+    
+    private long amount;
+    private int bankID;
+    
+    
+    public TransactionPageTest(Event event) {
+        
+        
+        
+        
+    }
+    
+    
+    
+}
