39fa0ca0d0a1b17e0d83c5e195146ad63526a3c3
epierrej <halodoctor3000@yahoo.com>
2017-12-02 12:29:00 -0600

    reuploading EventBriteDemo folder

---
+++ EventBriteDemo/target/test-classes/EventInfoTest.java
@@ -0,0 +1,61 @@
+//package com.EventBrite.EventBriteDemo.JUnitTestcases;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EventInfoTest {
+	ReadInData librarian;
+	EventPage eventPage;
+	@Before
+	public void Setup() {
+		
+		librarian = new ReadInData();
+		librarian.InitEventData();
+		librarian.InitUserData();
+		eventPage = new EventPage(librarian, librarian.getTheUsers().get(1));
+	}
+	
+	@Test
+	public void getTitle() {
+		assertEquals("Hound Town",librarian.getTheEvents().get(1).getTitle());
+	}
+	@Test
+	public void getDay() {
+		assertEquals("13",librarian.getTheEvents().get(1).getDay());
+	}
+	@Test
+	public void getMonth() {
+		assertEquals("9",librarian.getTheEvents().get(1).getMonth());
+	}
+	@Test
+	public void getState() {
+		assertEquals("TX",librarian.getTheEvents().get(1).getState());
+	}
+	@Test
+	public void getBuildingNum() {
+		assertEquals("96942",librarian.getTheEvents().get(1).getBuildingNum());
+	}
+	@Test
+	public void getStreet() {
+		assertEquals("Scott St.",librarian.getTheEvents().get(1).getStreet());
+	}
+	@Test
+	public void getTime() {
+		assertEquals("9:00",librarian.getTheEvents().get(1).getTime());
+	}
+	@Test
+	public void getPrice() {
+		assertEquals("10.50",librarian.getTheEvents().get(1).getTicketPrice());
+	}
+	@Test
+	public void getCity() {
+		assertEquals("Houston",librarian.getTheEvents().get(1).getCity());
+	}
+	@Test
+	public void EventToDetails() {
+		eventPage.WeAreTesting();
+	}
+
+}
